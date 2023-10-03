8dbd94d83bfe573b8cc29f9f203fb203d491ffba
Felipe da Silva Martins <femartins2010@gmail.com>
2023-10-02 23:09:30 -0300

    resolução do exercicio

--- src/exercicio.js
+++ src/exercicio.js
@@ -1,34 +1,10 @@
-const alunos = [
-    {
-        nome: "felipe",
-        nota: 9
-    },
-    {
-        nome: "gabriela",
-        nota: 10
-    },
-    {
-        nome: "serjao dos foguetes",
-        nota: 6
-    },
-    {
-        nome: "rikelme",
-        nota: 7
-    },
-    {
-        nome: "shaolim matador de porco",
-        nota: 5
-    },
-    {
-        nome: "flavin do pneu",
-        nota: 3
+"use strict";
+var exercicio;
+(function (exercicio) {
+    function multiplica(valor1, valor2) {
+        return valor1 * valor2;
     }
-]
-
-function alunosAprovados(baseAlunos){
-    return baseAlunos.filter((aluno) =>{
-        return aluno.nota >= 6
-    })
-}
-
-console.log(alunosAprovados(alunos))
+    function saudar(nome) {
+        return `Olá ${nome}`;
+    }
+})(exercicio || (exercicio = {}));
---
+++ src/exercicio.ts
@@ -0,0 +1,9 @@
+namespace exercicio{
+    function multiplica (valor1: number, valor2: number): number{
+        return valor1 * valor2
+    }
+
+    function saudar(nome:string): string{
+        return `Olá ${nome}`
+    }
+}
